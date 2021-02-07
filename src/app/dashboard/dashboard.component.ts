e39f934b0fecd76728d9b39729d3f0bf545542c6
sanas <sanashee05@hotmai.com>
2021-02-07 03:22:39 -0800

    set active mat tab

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import {MatTabChangeEvent} from '@angular/material/tabs';
+import {MatTab, MatTabChangeEvent} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,32 +11,51 @@ import {MatTabChangeEvent} from '@angular/material/tabs';
 })
 export class DashboardComponent implements OnInit {
 
-  tabSelected = 1;
-  tabName = "tab1";
+  tabSelected = 3;
+  tabName = "tab3";
 
-  constructor(private authService : AuthService, private router: Router, private afAuth: AngularFireAuth) { }
+  index = 2
+
+  constructor(private authService : AuthService, private router: Router, private afAuth: AngularFireAuth) { 
+
+    // get data to see what program use is in or not in any and set these values 
+    this.tabSelected = 2;
+    this.tabName = "tab2";
+    this.index = 1
+   
+  }
 
   ngOnInit(): void {}
+
+  getSelectedIndex(): number {
+
+    return this.index
+}
 
   async openSettings(){
     this.router.navigate(['settings']);
   }
 
   async onTabChange(event: MatTabChangeEvent) {
+    console.log("yoyo: " + event.tab);
+    console.log("here is the event: " + event)
     this.tabName = event.tab.textLabel;   
     
 
     switch(this.tabName) { 
       case "tab1": { 
         this.tabSelected = 1
+        this.index = 0
         break; 
       } 
       case "tab2": { 
         this.tabSelected = 2
+        this.index = 1
         break; 
       } 
       case "tab3": { 
         this.tabSelected = 3
+        this.index = 2
         break; 
      }  
       default: { 
