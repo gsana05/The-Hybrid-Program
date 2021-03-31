88813d7074417c33e5864c3789867ab12289e891
sanas <sanashee05@hotmai.com>
2021-03-31 13:28:33 -0700

    tidy up graphs

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -226,27 +226,23 @@ hasGraphData = false;
         var myChart = new Chart("barChart", {
           type: 'bar',
           data: {
-            labels: ['Test', 'Stretch', 'Running', 'Resistance', 'Hybrid', 'Rest', 'Walking'],
+            labels: ['Test', 'Running', 'Resistance', 'Hybrid', 'Rest'],
             datasets: [{
               label: '',
-              data: [this.workoutTestCount, this.stretchAndCoreCount, this.runningCount, this.resistanceCount, this.runningAndResistanceCount, this.restCount, this.walkingAndResistanceCount],
+              data: [this.workoutTestCount, this.runningCount, this.resistanceCount, this.runningAndResistanceCount, this.restCount],
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgb(60, 179, 113, 0.2)'
+                'rgba(255, 159, 64, 0.2)'
               ],
               borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgb(0,128,0, 1)'
+                'rgba(255, 159, 64, 1)'
               ],
               borderWidth: 1
             }]
